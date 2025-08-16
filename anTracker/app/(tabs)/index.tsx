afc5333dcb1822ad1bc10c2fb37b9f243db107b1
Froggotkd <djojavi19@gmail.com>
2025-08-16 18:30:11 -0500

    feature(index): add welcome page

--- anTracker/app/(tabs)/index.tsx
+++ anTracker/app/(tabs)/index.tsx
@@ -1,110 +1,81 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { useCategorias } from '@/hooks/useCategorias';
-import { useEffect, useState } from 'react';
+//Pantalla de bienvenida
 
-export interface Categoria {
-  categoria_id?: number,
-  categoria_nombre: string,
-  categoria_color: string
-}
-
-export default function HomeScreen() {
-  const { addCategoria, getCategorias } = useCategorias();
-  const [categorias, setCategorias] = useState<Categoria[]>([]);
-  let isMounted = true;
-
-  const initializeData = async () => {
-    try {
-      const data = await getCategorias();
-      if (isMounted) {
-        setCategorias(data);
-        console.log(data)
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
-  useEffect(() => {
-    initializeData();
-    console.log(categorias)
-    return () => { isMounted = false };
-  }, []);
+const Welcome = () => {
+  const navigation = useNavigation();
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1</ThemedText>
-        <ThemedText type="subtitle">
-          {categorias.length > 0
-            ? categorias.map((c, i) => `${c.categoria_color} (${c.categoria_nombre})`).join(', ')
-            : 'No hay categorías'}
-        </ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <View style={styles.images}>
+        <Image source={require('../../assets/images/LogoWelcome.png')} style={{ marginTop: 150, width: 320, height: 115 }} />
+        <Image source={require('../../assets/images/AntWelcome.png')} style={{ marginTop: 50, width: 210, height: 190 }} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.buttonTransacciones}>
+          <Text style={styles.buttonTitle}>Transacciones</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonTitle}>Categorias</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  buttonTransacciones: {
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
+    backgroundColor: '#A37366',
+    height: 40,
+    width: 200,
+    borderColor: '#A37366',
+    borderRadius: 10
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  images: {
+    alignItems: 'center'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
+  txt: {
+    textAlign: 'left',
+    marginTop: 150,
+    fontWeight: '600',
+    fontSize: 20,
+    fontStyle: 'italic',
+    marginBottom: 10
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#E0F7FA'
+  },
+  buttonContainer: {
+    flexDirection: 'column',
     position: 'absolute',
+    bottom: 50,
+    width: '80%',
+    alignItems: 'center',
+    borderRadius: 10,
+    overflow: 'hidden',
+    gap: 20,
+    justifyContent: 'center',
+    marginBottom: 45,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#A37366',
+    height: 40,
+    width: 150,
+    borderColor: '#A37366',
+    borderRadius: 10
+  },
+  buttonTitle: {
+    fontSize: 16,
+    color: '#FFFFFF',
   },
 });
+
+export default Welcome;
--- anTracker/app/_layout.tsx
+++ anTracker/app/_layout.tsx
@@ -1,6 +1,5 @@
 import { Stack } from 'expo-router';
 import { SQLiteProvider } from 'expo-sqlite';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { useDatabase } from '../hooks/useDatabase';
@@ -19,9 +18,8 @@ export default function RootLayout() {
   return (
     <SQLiteProvider databaseName='antracker.db'>
       <DatabaseInitializer />
-      <StatusBar style="auto" />
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }}  />
         <Stack.Screen name="+not-found" />
       </Stack>
     </SQLiteProvider>
